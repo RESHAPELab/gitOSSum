afadd441b8367e26b64bfcac2782bc293bb7601b
Stephen White <Stevewhite.dev@gmail.com>
2019-01-22 15:08:44 -0700

    Can find a repo's main json api page in mongo
    * We can query our database and find a repository's main json api page
    * by simply searching for the owner/repo
    
    * Added modifications to allow the user to spell the name of the repo in
    * any case and still find the repo within our database
    
    * Going to add a function that does all mining operations in one call

--- mining_scripts/mining.py
+++ mining_scripts/mining.py
@@ -8,7 +8,6 @@
 
 from pymongo import MongoClient
 from github import Github
-import json
 
 
 client = MongoClient('localhost', 27017)
@@ -16,7 +15,7 @@ db = client.backend_db
 repos = db.repos
 pull_requests = db.pullRequests
 g = Github("Githubfake01", "5RNsya*z#&aA", per_page=100)
-repo_name = "Swhite9478/Github-Mining-Tool"
+repo_name = "google/gumbo-parser"
 pygit_repo = g.get_repo(repo_name)
 
 # To find a specific repo: db.pullRequests.find({"url":{$regex: repo}})
@@ -24,7 +23,9 @@ pygit_repo = g.get_repo(repo_name)
 
 # Method to download a repo's main json and place it in the 
 # db.repos collection for future parsing 
-def mine_repo_page(repo):
+def mine_repo_page(repo_name):
+    # Use pygit to eliminate any problems with users not spelling the repo name
+    # exactly as it is on the actual repo 
     pygit_repo = g.get_repo(repo_name)
     repos.update(pygit_repo.raw_data, pygit_repo.raw_data, upsert=True)
     return 
@@ -44,16 +45,30 @@ def mine_pulls_from_repo(repo_name):
         pull_requests.update(pull.raw_data, pull.raw_data, upsert=True)
 
 
+# Helper method to find a specific repo's main api page json 
+def find_repo_main_page(repo_name):
+    # Use pygit to eliminate any problems with users not spelling the repo name
+    # exactly as it is on the actual repo 
+    pygit_repo = g.get_repo(repo_name)
+    print(repos.find_one({"full_name":pygit_repo.full_name}))
+    return repos.find_one({"full_name":pygit_repo.full_name})
+
+
 # Helper method to find and return all pull request json files 
 # belonging to a specific repo 
-def find_all_repo_pulls(repo):
-    print(repo)
-    pulls = pull_requests.find({"url": {"$regex": repo}})
+def find_all_repo_pulls(repo_name):
+    # Use pygit to eliminate any problems with users not spelling the repo name
+    # exactly as it is on the actual repo 
+    pygit_repo = g.get_repo(repo_name)
+
+    # Obtain a list of all the pull requests matching the repo's full name 
+    pulls = pull_requests.find({"url": {"$regex": pygit_repo.full_name}})
     for pull in pulls:
         print(pull)
     return pulls
 
 
-mine_repo_page(repo_name)
+# mine_repo_page(repo_name)
+find_repo_main_page(repo_name)
 # mine_pulls_from_repo(repo_name)
 # find_all_repo_pulls(repo_name)
