9b5d069b3d7a5b7128aebfc40dd5b16670cbcc50
Stephen White <Stevewhite.dev@gmail.com>
2019-01-22 15:26:14 -0700

    Added wrapper function to take care of all mining
    * Added mine_and_store_all_repo_data() to take care of all mining tasks
    * from a single method call
    
    * I think it is time to add some tests to always ensure we are running
    * correctly!

--- mining_scripts/mining.py
+++ mining_scripts/mining.py
@@ -10,33 +10,45 @@ from pymongo import MongoClient
 from github import Github
 
 
-client = MongoClient('localhost', 27017)
-db = client.backend_db
-repos = db.repos
-pull_requests = db.pullRequests
-g = Github("Githubfake01", "5RNsya*z#&aA", per_page=100)
-repo_name = "google/gumbo-parser"
-pygit_repo = g.get_repo(repo_name)
+client = MongoClient('localhost', 27017) # Where are we connecting
 
-# To find a specific repo: db.pullRequests.find({"url":{$regex: repo}})
+db = client.backend_db # The specific mongo database we are working with 
+
+repos = db.repos # collection for storing all of a repo's main api json information 
+
+pull_requests = db.pullRequests # collection for storing all pull requests for all repos 
+
+g = Github("Githubfake01", "5RNsya*z#&aA", per_page=100) # authorization for the github API 
+
+TEST_REPO = "swhite9478/github-mining-tool" # repo for testing purposes 
+
+
+# Wrapper function that will perform all mining steps necessary when
+# provided with the repository name
+def mine_and_store_all_repo_data(repo_name):
+    # Use pygit to eliminate any problems with users not spelling the repo name
+    # exactly as it is on the actual repo 
+    pygit_repo = g.get_repo(repo_name)
+
+    # mine and store the main page josn
+    mine_repo_page(pygit_repo)
+
+    # mine and store all pulls for this repo 
+    mine_pulls_from_repo(pygit_repo)
+    return 
+
 
 
 # Method to download a repo's main json and place it in the 
 # db.repos collection for future parsing 
-def mine_repo_page(repo_name):
-    # Use pygit to eliminate any problems with users not spelling the repo name
-    # exactly as it is on the actual repo 
-    pygit_repo = g.get_repo(repo_name)
+def mine_repo_page(pygit_repo):
     repos.update(pygit_repo.raw_data, pygit_repo.raw_data, upsert=True)
     return 
 
 
 # Method to download all pull requests of a given repo and 
 # put them within the db.pullRequests collection 
-def mine_pulls_from_repo(repo_name):
-    # Use pygit to retrieve the repo itself 
-    pygit_repo = g.get_repo(repo_name)
-
+def mine_pulls_from_repo(pygit_repo):
     # Retrieve all pull request numbers associated with this repo 
     pulls = pygit_repo.get_pulls('all')
 
@@ -44,17 +56,19 @@ def mine_pulls_from_repo(repo_name):
         # Overwrite already existing instances of jsons 
         pull_requests.update(pull.raw_data, pull.raw_data, upsert=True)
 
+    return 
+
 
 # Helper method to find a specific repo's main api page json 
 def find_repo_main_page(repo_name):
     # Use pygit to eliminate any problems with users not spelling the repo name
     # exactly as it is on the actual repo 
     pygit_repo = g.get_repo(repo_name)
-    print(repos.find_one({"full_name":pygit_repo.full_name}))
+    
     return repos.find_one({"full_name":pygit_repo.full_name})
 
 
-# Helper method to find and return all pull request json files 
+# Helper method to find and return a list of all pull request json files 
 # belonging to a specific repo 
 def find_all_repo_pulls(repo_name):
     # Use pygit to eliminate any problems with users not spelling the repo name
@@ -63,12 +77,10 @@ def find_all_repo_pulls(repo_name):
 
     # Obtain a list of all the pull requests matching the repo's full name 
     pulls = pull_requests.find({"url": {"$regex": pygit_repo.full_name}})
-    for pull in pulls:
-        print(pull)
+
     return pulls
 
 
-# mine_repo_page(repo_name)
-find_repo_main_page(repo_name)
-# mine_pulls_from_repo(repo_name)
-# find_all_repo_pulls(repo_name)
+mine_and_store_all_repo_data(TEST_REPO)
+# find_repo_main_page(TEST_REPO)
+# find_all_repo_pulls(TEST_REPO)
