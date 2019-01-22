eab4365e0a8fea5fd92c7896ebcb7c81395fa40a
Stephen White <Stevewhite.dev@gmail.com>
2019-01-22 14:26:15 -0700

    We can mine all pull requests from a repo now
    * Made some changes and checked against github's website to ensure all
    * pull requests are indeed being mined, and this is true.
    
    * Now to mine the repo's main page

--- mining_scripts/mining.py
+++ mining_scripts/mining.py
@@ -13,22 +13,27 @@ import json
 
 client = MongoClient('localhost', 27017)
 db = client.backend_db
+repos = db.repos
 pull_requests = db.pullRequests
-g = Github("Githubfake01", "5RNsya*z#&aA")
-repo = g.get_repo("Swhite9478/Github-Mining-Tool")
+g = Github("Githubfake01", "5RNsya*z#&aA", per_page=100)
+repo_name = "google/gumbo-parser"
+pygit_repo = g.get_repo(repo_name)
 
 # To find a specific repo: db.pullRequests.find({"url":{$regex: repo}})
 
+
 # Method to download all pull requests of a given repo and 
 # put them within the db.pullRequests collection 
-def mine_pulls_from_repo(repo):
+def mine_pulls_from_repo(repo_name):
+    # Use pygit to retrieve the repo itself 
+    pygit_repo = g.get_repo(repo_name)
 
     # Retrieve all pull request numbers associated with this repo 
-    pulls = repo.get_pulls(state='all', sort='created', base='master')
+    pulls = pygit_repo.get_pulls('all')
 
-    raw_json = repo.get_pull(pulls[0].number).raw_data
-
-    pull_requests.update(raw_json, raw_json, upsert=True)
+    for pull in pulls:
+        # Overwrite already existing instances of jsons 
+        pull_requests.update(pull.raw_data, pull.raw_data, upsert=True)
 
 
 # Helper method to find and return all pull request json files 
@@ -39,7 +44,6 @@ def find_all_repo_pulls(repo):
     for pull in pulls:
         print(pull)
     return pulls
-    
-# mine_pulls_from_repo(repo)
 
-find_all_repo_pulls(repo.full_name)
+mine_pulls_from_repo(repo_name)
+# find_all_repo_pulls(repo_name)
