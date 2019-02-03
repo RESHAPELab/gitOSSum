acaba2b77b7b2a09b2e3dedf3d92eaf454774d3c
Stephen White <Stevewhite.dev@gmail.com>
2019-02-02 21:48:29 -0700

    User Authentication is Complete

--- src/web_app/user_app/views.py
+++ src/web_app/user_app/views.py
@@ -18,19 +18,35 @@ import random
 
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
-
-
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import user_passes_test
+from django.conf import settings
 
 # MongoDB information 
 client = MongoClient('localhost', 27017)
 db = client.test_database
 pull_requests = db.pullRequests
 
+LOGIN_REDIRECT_URL = settings.LOGIN_REDIRECT_URL
+
+def login_forbidden(function=None, redirect_field_name=None, redirect_to=LOGIN_REDIRECT_URL):
+    """
+    Decorator for views that checks that the user is NOT logged in, redirecting
+    to the homepage if necessary.
+    """
+    actual_decorator = user_passes_test(
+        lambda u: not u.is_authenticated(),
+        login_url=redirect_to,
+        redirect_field_name=redirect_field_name
+    )
+    if function:
+        return actual_decorator(function)
+    return actual_decorator
 
 class HomeView(TemplateView):
     template_name = 'home.html'
 
-
+@login_forbidden
 def signup(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
@@ -49,6 +65,8 @@ def signup(request):
         form = SignUpForm()
     return render(request, 'signup.html', {'form': form})
 
+
+@login_required
 def mining_request_form_view(request):
     context = {}
     requests = MiningRequest.objects.all()
