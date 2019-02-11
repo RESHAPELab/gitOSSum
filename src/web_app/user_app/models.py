433ddbb94817441dae4d484cf59165c40380b148
Stephen White <Stevewhite.dev@gmail.com>
2019-02-11 11:14:18 -0700

    adding new SQL models for visualization

--- src/web_app/user_app/models.py
+++ src/web_app/user_app/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from pymongo import MongoClient
+from django_mysql.models import ListTextField
+from django.core.validators import MinValueValidator
 
 class OAuthToken(models.Model):
     oauth_token             = models.CharField(max_length=240, null=False, blank=False)
@@ -29,10 +31,14 @@ class BlacklistedMiningRequest(models.Model):
 class MinedRepo(models.Model):
     repo_name                    = models.CharField(max_length=240, null=False, blank=False)
     requested_by                 = models.CharField(max_length=240, null=False, blank=False)
-    #num_pulls                    = models.IntegerField(min_value=0)
-    #num_closed_merged_pulls      = models.IntegerField(min_value=0)
-    #num_closed_unmerged_pulls    = models.IntegerField(min_value=0)
-    #num_open_pulls               = models.IntegerField(min_value=0)
+    num_pulls                    = models.IntegerField(validators=[MinValueValidator(0)])
+    num_closed_merged_pulls      = models.IntegerField(validators=[MinValueValidator(0)])
+    num_closed_unmerged_pulls    = models.IntegerField(validators=[MinValueValidator(0)])
+    num_open_pulls               = models.IntegerField(validators=[MinValueValidator(0)])
+    created_at_list              = ListTextField(base_field=models.CharField(max_length=240))
+    closed_at_list               = ListTextField(base_field=models.CharField(max_length=240))
+    merged_at_list               = ListTextField(base_field=models.CharField(max_length=240))
+    num_newcomer_labels          = models.IntegerField(validators=[MinValueValidator(0)])
     timestamp                    = models.DateTimeField(auto_now_add=True)
     updated                      = models.DateTimeField(auto_now=True)
 
